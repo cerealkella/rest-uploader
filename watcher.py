8c0d5ba07ef3c14db5e0c7957ce5b9f865793480
kellerjustin <kellerjustin@gmail.com>
2018-09-24 17:22:59 -0500

    0.1

---
+++ watcher.py
@@ -0,0 +1,86 @@
+import sys
+import os
+import platform
+import time
+import base64
+import magic
+from requests import post
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+'''
+2018-09-24 JRK
+This program was created to upload files from a folder specified in the
+PATH variable to Joplin. The following resource was helpful in figuring out
+the logic for Watchdog:
+https://stackoverflow.com/questions/18599339/python-watchdog-monitoring-file-for-changes
+
+The API seems to only support image file uploads, PDFs do not work.
+I'd like to build out fully functional support for .md and .txt files
+but I don't presently have time to fix the issue with the API and handling
+of quotes and double-quotes
+'''
+
+
+SERVER = 'http://127.0.0.1:41184/notes'
+PATH = './upload/'
+
+
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(event.event_type + " -- " + event.src_path)
+        upload(event.src_path)
+
+
+# Set working Directory
+def set_working_directory():
+    if os.getcwd() != os.chdir(os.path.dirname(os.path.realpath(__file__))):
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+
+def read_text_note(filename):
+    with open(filename, 'r') as myfile:
+        text = myfile.read()
+        print(text)
+    return text
+
+
+def upload(filename):
+    mime = magic.Magic(mime=True)
+    datatype = mime.from_file(filename)
+    encoded = base64.b64encode(open(filename, "rb").read())
+    img = "data:{};base64,{}".format(datatype, encoded.decode())
+    title = os.path.basename(filename)
+    if datatype == "text/plain":
+        # this is not working correctly yet
+        # will bomb if there are quotes in the text file
+        body = read_text_note(filename)
+        print(body)
+        values = '{{ "title": "{}", "body": "{}" }}'\
+            .format(title, repr(body).replace("'", ""))
+        print(values)
+        print(repr(values))
+    else:
+        body = filename + " uploaded from " + platform.node()
+        values = '{{ "title": "{}", "body": "{}", "image_data_url": "{}" }}'\
+            .format(title, body, img)
+    response = post(SERVER, data=values)
+    print(response)
+    print(response.text)
+    print(response.json())
+
+
+if __name__ == "__main__":
+    set_working_directory()
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=PATH, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
