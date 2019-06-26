754a003da412b06b8e0e5623600a526724026fc0
kellerjustin <kellerjustin@gmail.com>
2019-06-26 12:57:25 -0500

    fix url, version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/kellerjustin/rest_uploader",
-    version="0.1.0",
+    url="https://github.com/kellerjustin/rest-uploader",
+    version="0.2.0",
     zip_safe=False,
 )
