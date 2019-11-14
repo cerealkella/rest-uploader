d283758eba26710171a623fce0ff99db049b5658
kellerjustin <kellerjustin@gmail.com>
2019-11-14 11:19:19 -0600

    Bump version: 0.7.0 → 0.8.0

--- rest_uploader/__init__.py
+++ rest_uploader/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Justin Keller"""
 __email__ = "kellerjustin@protonmail.com"
-__version__ = "__version__ = '0.7.0'"
+__version__ = "__version__ = '0.8.0'"
--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kellerjustin/rest-uploader",
-    version="version='0.7.0'",
+    version="version='0.8.0'",
     zip_safe=False,
 )
