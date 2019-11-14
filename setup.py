269d58ac33dde2253985c9dfd7d4a11185db3b55
kellerjustin <kellerjustin@gmail.com>
2019-11-14 11:49:10 -0600

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kellerjustin/rest-uploader",
-    version="version='1.0.0'",
+    version="version='1.1.0'",
     zip_safe=False,
 )
