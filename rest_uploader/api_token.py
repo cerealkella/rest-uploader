c17b1899ffdc5bfb7ea4a5cf3434f0f44d0863d8
Justin <kellerjustin@protonmail.com>
2022-07-10 22:51:11 -0500

    refactors logic to put the token in userspace

--- rest_uploader/api_token.py
+++ rest_uploader/api_token.py
@@ -1,41 +1,54 @@
-from os import path
+import sys
+import pathlib
 
 
-def get_token():
-    if path.exists(".api_token.txt"):
-        with open(".api_token.txt", "r") as f:
+def get_os_datadir() -> pathlib.Path:
+    """
+    Returns a parent directory path
+    where persistent application data can be stored.
+
+    # linux: ~/.local/share
+    # macOS: ~/Library/Application Support
+    # windows: C:/Users/<USER>/AppData/Roaming
+    """
+    home = pathlib.Path.home()
+
+    if sys.platform == "win32":
+        return home / "AppData/Roaming"
+    elif sys.platform == "linux":
+        return home / ".local/share"
+    elif sys.platform == "darwin":
+        return home / "Library/Application Support"
+
+
+def get_my_datadir() -> pathlib.Path:
+    """Find datadir, create it if it doesn't exist"""
+    my_datadir = get_os_datadir() / __package__
+    try:
+        my_datadir.mkdir(parents=True)
+    except FileExistsError:
+        pass
+    return my_datadir
+
+
+def get_token() -> str:
+    """gets token from .api_token.txt file. If it doesn't exist,
+    prompt for the user to paste it in.
+
+    Returns:
+        str: token text
+    """
+    token = ""
+    token_file = get_my_datadir() / ".api_token.txt"
+    if token_file.exists():
+        with open(token_file, "r") as f:
             token = f.readline().rstrip()
     else:
         token = input("Paste your Joplin API Token:")
-        with open(".api_token.txt", "w") as f:
+        with open(token_file, "w") as f:
             f.write(token.rstrip())
     return token
 
 
 def get_token_suffix():
     return "?token=" + get_token()
-
-
-"""
-Code from a non-Joplin API
-Not currently used
-"""
-
-"""
-import requests
-
-def get_header():
-    try:
-        my_token = get_token()
-        url = SERVER + "/ping"
-        head = {"Authorization": f"Bearer {my_token}"}
-        try:
-            response = requests.get(url, headers=head)
-            if response.status_code != 200:
-                print("Invalid token!")
-        except ValueError:
-            print("Token expired!")
-    except FileNotFoundError:
-        print("No Token!")
-    return head
-"""
