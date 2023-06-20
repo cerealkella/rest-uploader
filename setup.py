0a20c399bf1af8738b962c2eeb3ec8c0d36cca9a
Justin <kellerjustin@protonmail.com>
2023-06-20 08:53:48 -0500

    Bump version: 1.17.0 → 1.18.0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kellerjustin/rest-uploader",
-    version="1.17.0",
+    version="1.18.0",
     zip_safe=False,
 )
